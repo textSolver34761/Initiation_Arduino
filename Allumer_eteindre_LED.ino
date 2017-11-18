df2d15f2241e5631eeaa90275a1223e70781cfc4
textSolver34761 <pradonbenjamin@gmail.com>
2017-11-18 11:09:11 +0100

    2eme commit

--- Allumer_eteindre_LED.ino
+++ Allumer_eteindre_LED.ino
@@ -1,22 +1,21 @@
 void setup() {
-  pinMode(5, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT); // Led position 5
+  pinMode(4, OUTPUT); // Led position 4
+  pinMode(3, OUTPUT); // Led position 3
 }
 
 void loop() {
-digitalWrite(5,HIGH);
-delay(500);
-digitalWrite(4,HIGH);
-delay(500);
-digitalWrite(3,HIGH);
-delay(500);
+digitalWrite(5,HIGH); // Allumer led position 5 ...
+delay(500);           // pendant 0.5 secondes
+digitalWrite(4,HIGH); // Allumer led position 4 ...
+delay(500);           //pendant 0.5 secondes
+digitalWrite(3,HIGH); // Allumer led position 3 ...
+delay(500);           // pendant 0.5 secondes
 
-
-digitalWrite(3,LOW);
-delay(500);
-digitalWrite(4,LOW);
-delay(500);
-digitalWrite(5,LOW);
-delay(500);
+digitalWrite(3,LOW); // Eteindre led position 3...
+delay(500);          //pendant 0.5 secondes
+digitalWrite(4,LOW); // Eteindre led position 4...
+delay(500);          //pendant 0.5 secondes
+digitalWrite(5,LOW); // Eteindre led position 5...
+delay(500);          //pendant 0.5 secondes
 }
